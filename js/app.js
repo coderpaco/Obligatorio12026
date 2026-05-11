9157706dbd0eea24df37f06ef81904c70cb5f8e6
coderpaco <templateadd@gmail.com>
2026-05-11 19:35:35 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+<!--
+window.addEventListener('load', inicio);
+function inicio(){
+  document.getElementById("boton").addEventListener("click", proceso);
+}
+function proceso(){
+  let texto = document.getElementById("frase").value;
+  let respuesta = pasarMayusculas(texto);
+  mostrarEnPantalla(respuesta);
+}
+-->
